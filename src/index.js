45d0cdda4c08e131be5e9f9d27b838f3f65d2317
KoVaL0 <goks223300@gmail.com>
2022-02-09 20:48:50 +0300

    feat(*): добавлены начальные файлы

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './styles.sass';
+
+// Просто создано для теста
+function component() {
+  const element = document.createElement('div');
+
+  return element;
+}
+
+document.body.appendChild(component());
