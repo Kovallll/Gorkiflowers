6c413defb9e83586e59a9a81210fe247ed5c83e0
KoVaL0 <56194853+KoVaL0@users.noreply.github.com>
2022-02-19 12:32:16 +0300

    feat(index.html,style.scss): upload header

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-import './styles.sass';
-
-// Просто создано для теста
-function component() {
-  const element = document.createElement('div');
-
-  return element;
-}
-
-document.body.appendChild(component());
+import './styles.scss';
